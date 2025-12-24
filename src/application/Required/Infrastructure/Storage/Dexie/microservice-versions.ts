9d4a51cf4dc95bd93fb157f85ca701c8120bc365
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-12-24 14:43:37 +0100

    Adds Thought infrastructure

--- src/application/Required/Infrastructure/Storage/Dexie/microservice-versions.ts
+++ src/application/Required/Infrastructure/Storage/Dexie/microservice-versions.ts
@@ -3,6 +3,9 @@ import Dexie from 'dexie'
 export const microserviceDb = new Dexie('MicroserviceDb')
 
 export async function applyMicroserviceVersions(): Promise<void> {
-  // Add future platform-level versions here
-  await Promise.resolve()
+  microserviceDb.version(1).stores({
+    thought: 'id, content, created_at, discarded_at, status',
+  })
+
+  await microserviceDb.open()
 }
