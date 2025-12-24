838d41afeacaa9b8099b646496f8341a2380b867
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-12-24 14:55:39 +0100

    Updates TopSection

--- src/application/Required/Application/Layout/TopSection.vue
+++ src/application/Required/Application/Layout/TopSection.vue
@@ -1,27 +1,15 @@
 <template>
-  <q-header elevated>
+  <q-header flat class="bg-white text-black no-border no-shadow">
     <NoticeComponent />
-
     <q-toolbar>
-      <!-- App name -->
-      <q-toolbar-title class="row items-center no-wrap">
-        <span>{{ PlatformConfig.applicationName }}</span>
-
-        <!-- Vertical divider -->
-        <q-separator vertical inset class="q-mx-md" />
-
-        <!-- Navigation links -->
-        <q-btn flat dense label="Documentation" to="/" />
-
-        <q-separator vertical inset class="q-mx-sm" />
-
-        <q-btn flat dense label="Example" to="/example/task" />
-      </q-toolbar-title>
+      <q-toolbar-title>{{ PlatformConfig.applicationName }}</q-toolbar-title>
+      <q-btn round flat icon="account_circle" :disable="true" />
     </q-toolbar>
   </q-header>
 </template>
 
 <script setup lang="ts">
   import NoticeComponent from 'src/application/Platform/Notice/Application/NoticeComponent.vue'
+
   import PlatformConfig from 'src/config/platform-config'
 </script>
