b04d211839342c306aaa7d1fa59fefde95a4eb03
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-12-24 14:54:34 +0100

    Adds UI wrapper

---
+++ src/application/Microservice/Thought/Application/Home/ThoughtsComponent/Components/Components/ThoughtsWrapper.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row q-col-gutter-md">
+    <div v-for="(item, index) in items" :key="index" class="col-xs-12 col-sm-6 col-md-4">
+      <slot :item="item" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts" generic="T">
+  defineProps<{
+    items: readonly T[]
+  }>()
+</script>
