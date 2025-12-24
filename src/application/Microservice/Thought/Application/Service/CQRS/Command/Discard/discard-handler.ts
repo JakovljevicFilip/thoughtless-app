381fde972fcffe41255248cac26cfd5d9cd224ad
Filip Jakovljevic <filip.jakovljevic@hotmail.com>
2025-12-24 14:45:01 +0100

    Adds Thought discard

---
+++ src/application/Microservice/Thought/Application/Service/CQRS/Command/Discard/discard-handler.ts
@@ -0,0 +1,37 @@
+import type { ThoughtDiscardPayload } from './ThoughtDiscardPayload'
+import { thoughtDiscardCommand } from './discard-command'
+
+import type { Thought } from 'src/application/Microservice/Thought/Domain/Thought'
+
+import { logger } from 'src/application/Platform/Log/Application/log-service'
+import type { LogAdapter } from 'src/application/Platform/Log/Domain/Log'
+import type { Command } from 'src/application/Platform/Service/Domain/CQRS/Command/Command'
+
+class ThoughtDiscardHandler {
+  private readonly log: LogAdapter
+  private readonly command: Command
+
+  constructor(log: LogAdapter, command: Command) {
+    this.log = log
+    this.command = command
+  }
+
+  async discard(thought: Thought, numberOfDiscardedThoughts: number): Promise<void> {
+    try {
+      const payload = <ThoughtDiscardPayload>{
+        thought: thought,
+        numberOfDiscardedThoughts: numberOfDiscardedThoughts,
+      }
+      await this.command.command(payload)
+    } catch (error) {
+      this.log.write({
+        context: 'Thought.discard',
+        thought,
+        error,
+      })
+      throw error
+    }
+  }
+}
+
+export const thoughtDiscardHandler = new ThoughtDiscardHandler(logger, thoughtDiscardCommand)
